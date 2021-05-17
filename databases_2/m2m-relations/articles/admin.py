0ae525288de1e6e7ca381406553be86daadde380
Marina Lobina <mvlobina@gmsil.com>
2021-05-18 00:48:48 +0300

    task_1

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, ArticleScope
 
-from .models import Article
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_count = 0
+        for form in self.forms:
+            if main_count > 1:
+                raise ValidationError('Можно указать только один основной раздел')
+            if form.cleaned_data and form.cleaned_data['is_main']:
+                main_count += 1
+        if main_count == 0:
+            raise ValidationError('Добавьте основной раздел')
+        return super().clean()
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    extra = 3
+    formset = ArticleScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInline]
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
     pass
--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 class Article(models.Model):
 
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
+    title = models.CharField(max_length=256, unique=True, verbose_name='Название')
+    text = models.TextField(unique=True, verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scopes = models.ManyToManyField('Scope',
+                                  verbose_name='Разделы',
+                                  related_name='articles',
+                                  through='ArticleScope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +18,40 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+
+    topic = models.TextField(unique=True, verbose_name='Название раздела')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.topic
+
+
+
+class ArticleScope(models.Model):
+
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False)
+
+    scope = models.ForeignKey(
+        Scope,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False)
+
+    is_main = models.BooleanField(verbose_name='Основной раздел', default=False)
+
+    class Meta:
+        ordering = ['-is_main']
+
+    def __str__(self):
+        return f'Статья {self.article} из раздела {self.scope}'
+
